e7a83a0deac408ed9658c4ab8ef7aa0709325b96
冯骥骅 <fengjihua@28kj.net>
2019-08-23 14:51:32 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, from } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+  title = 'async-app';
+
+  minute = 0;
+
+  ngOnInit(): void {
+    // *0
+    console.log('[5分钟GET H5] - 被玩坏了的异步处理 RxJS');
+    // 煮蛋器
+    const eggMachine = Observable.create((xiaoMing) => {
+      console.log('8点' + this.minute + '分 煮鸡蛋');
+      this.minute += 8;
+      xiaoMing.next('8点' + this.minute + '分 [鸡蛋完成]');
+    });
+
+    // 面包机
+    const toastMachine = Observable.create((xiaoMing) => {
+      console.log('8点' + this.minute + '分 烤面包');
+      this.minute += 6;
+      xiaoMing.next('8点' + this.minute + '分 [面包完成]');
+    });
+
+    // 咖啡机
+    const cafeMachine = Observable.create((observer) => {
+      console.log('8点' + this.minute + '分 煮咖啡');
+      this.minute += 2;
+      observer.next('8点' + this.minute + '分 [咖啡完成]');
+    });
+
+    // 同步
+    console.log('[5分钟GET H5] - 同步的第一天：');
+    this.minute = 0;
+    eggMachine.subscribe((message) => {
+      console.log(message);
+
+      cafeMachine.subscribe((message) => {
+        console.log(message);
+
+        toastMachine.subscribe((message) => {
+          console.log(message);
+        });
+      });
+    });
+
+    // 异步
+    console.log('[5分钟GET H5] - 异步的第二天：');
+    this.minute = 0;
+    eggMachine.subscribe((message) => {
+      console.log(message);
+    });
+
+    this.minute = 0;
+    cafeMachine.subscribe((message) => {
+      console.log(message);
+    });
+
+    this.minute = 0;
+    toastMachine.subscribe((message) => {
+      console.log(message);
+    });
+  }
+}
